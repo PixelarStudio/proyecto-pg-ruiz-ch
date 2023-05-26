3157bce94a34225061b537c238b98d8e670536ab
Cristian Ruiz <cjruiz2013@gmail.com>
2023-05-26 18:21:57 -0300

    Version Actualizada: Se modifica la estructura y se agregan operaciones y se separa en diferentes funciones. se utiliza un switch, se ejectua desde index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function bienvenida() {
+/*function bienvenida() {
 
     alert("Bienvenid@ a Pro Gamer Arg.");
     alert("Sistema de Ventas al por Mayor de Equipos Informaticos (Listas para usar.)");
@@ -71,5 +71,66 @@ function bienvenida() {
 }
 bienvenida() 
 
+//Pre-Entrega: --- Entrega modificada --- 👇👇👇
 
+*/
 
+//Inicia los mensajes de informacion del proyecto. 
+function mostrarMensaje(mensaje) {
+  alert(mensaje);
+}
+//obtiene la cantidad que se desee ingresar para la compra.
+function obtenerCantidad() {
+  return parseInt(prompt("Ingrese la cantidad deseada", 0));
+}
+//Calcula el precio en base a la cantidad y aplica el iva correspondiente. 
+function calcularPrecioFinal(precio, cantidad) {
+  let descuento1 = 0.1;
+  const IVA = 0.21;
+  let precioFinal = (precio + IVA * precio) - (descuento1 * precio);
+  return precioFinal * cantidad;
+}
+//Obtenido el precio y la cantidad comienza el proceso de finalizacion de compra, informando el importe final con iva aplicado. 
+function procesarPedido(precio, cantidad) {
+  let precioFinal = calcularPrecioFinal(precio, cantidad);
+  alert(`Gracias por su compra. Su pedido está siendo procesado. El precio final es de $${precioFinal}.`);
+}
+//Se ejecuta la funciona la iniciar el sitio desde index.html una vez finalizado el proceso de seleccion con switch, se puede navegar por el sitio. 
+function bienvenida() {
+  mostrarMensaje("Bienvenid@ a Pro Gamer Arg.");
+  mostrarMensaje("Sistema de Ventas al por Mayor de Equipos Informáticos.");
+  mostrarMensaje("Arma tu pedido seleccionando la opción que más te guste. Todos los productos cuentan con 10% de Descuento.");
+
+  let opciones = parseInt(prompt("Precios expresados sin IVA \n1) PC INTEL: $150.000, \n2) PC AMD: $180.000, \n3) COMBO ACTUALIZACIÓN: $55.000"));
+
+  switch (opciones) {
+    case 1:
+      mostrarMensaje("Seleccione la cantidad de PCs INTEL que desea comprar 🖥");
+      let cantidad1 = obtenerCantidad();
+      procesarPedido(150000, cantidad1);
+      break;
+    case 2:
+      mostrarMensaje("Seleccione la cantidad de PCs AMD que desea comprar 🖥");
+      let cantidad2 = obtenerCantidad();
+      procesarPedido(180000, cantidad2);
+      break;
+    case 3:
+      mostrarMensaje("Seleccione la cantidad de COMBOS DE ACTUALIZACIÓN que desea comprar 🖥🖱");
+      let cantidad3 = obtenerCantidad();
+      procesarPedido(55000, cantidad3);
+      break;
+    default:
+      const eleccion = confirm("¿Desea volver al principio?");
+      if (eleccion) {
+        bienvenida();
+      } else {
+        return "🛑⚠ Por favor, seleccione una opción válida ⚠🛑";
+      }
+  }
+
+  if (confirm("¿Te gustaría seguir comprando?")) {
+    bienvenida();
+  }
+}
+
+bienvenida()
